8a40ae74b1e863ca4e1b438351ff4bccc1c7f6d3
adulguun0418 <adulguun0418@gmail.com>
2021-10-27 10:51:14 +0900

    Second commit

--- program.rb
+++ program.rb
@@ -1,5 +1,11 @@
 for num in 1..100
   if num % 3 == 0 && num % 5 == 0
     puts "FizzBuzz\n"
-  elsif num % 3 ==0 
+  elsif num % 3 == 0 
     puts "Fizz\n"
+  elsif num % 5 == 0
+    puts "Buzz\n"
+  else 
+    puts num + "\n"
+  end
+end
